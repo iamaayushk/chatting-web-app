9665835f770df043abef3cc703790fbbb2d8c9ec
imaayushk <kumaraayush327@gmail.com>
2025-07-28 02:18:31 +0530

    added little websocket

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from 'react'
+import { io } from 'socket.io-client';
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
-
+  const socket= io("http://localhost:3000");
+  useEffect(() => {
+    socket.on("connect",()=>{
+      console.log("connected", socket.id);
+      
+    })
+    socket.on("welcome",(s)=>{
+      console.log(s);
+      
+    })
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex justify-center items-center flex-col gap-5'>
+      <h1 className='text-sky-400 text-center text-2xl font-bold'>Chatify</h1>
+      <h3 className='text-sky-300 font-semibold'>Type your message</h3>
+
+      <input
+        type='text'
+        className='border rounded-lg p-2 font-semibold '
+        placeholder='enter your message'
+      />
+    </div>
   )
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -3,15 +3,40 @@
     const connectDB= require('./config/db');
     const dotenv = require('dotenv');
     const cors= require('cors');
+    const {Server} = require('socket.io');
 
     const userRoutes = require('./routes/userRoutes');
     dotenv.config();
     connectDB();
-
     const app= express();
-    const server = http.createServer(app);
-    app.use(cors());
     app.use(express.json());
+
+    const server = http.createServer(app);
+    const io= new Server(server,{
+        cors:{
+            origin:"http://localhost:5173",
+            methods:["GET","POST"],
+            credentials: true,
+        }
+    });
+
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+            methods: ["GET", "POST"],
+            credentials: true,
+        })
+    );
+    io.on("connection",(socket)=>{
+        console.log("user connected");
+        console.log("userid", socket.id);
+        // socket.emit("welcome",`welcome to the server ${socket.id}`);
+        socket.broadcast.emit("welcome",`welcome to the server ${socket.id}`);//this will send to all users except the one who connected
+        
+    })
+
+
+   
 
     app.use('/api', userRoutes);
     const PORT = process.env.PORT;

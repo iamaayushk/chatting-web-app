73b9b4880ce6c8cd0b58d7ef8553a9781b9c7c60
imaayushk <kumaraayush327@gmail.com>
2025-07-29 19:06:29 +0530

    websocket trial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,44 @@
-import React, { useEffect } from 'react'
-import { io } from 'socket.io-client';
+import React, { useEffect, useMemo, useState } from 'react'
+import {io} from "socket.io-client";
 const App = () => {
+  const socket = useMemo(()=>io('http://localhost:3000'), []);
+  const [message, setMessage] = useState('');
 
-  const socket= io("http://localhost:3000");
   useEffect(() => {
     socket.on("connect",()=>{
-      console.log("connected", socket.id);
+      console.log("user connected", socket.id);
+    });
+    socket.on("receive-message",(data)=>{
+      console.log("message in client ", data);;
       
     })
-    socket.on("welcome",(s)=>{
-      console.log(s);
-      
-    })
-  }, []);
-  return (
-    <div className='flex justify-center items-center flex-col gap-5'>
-      <h1 className='text-sky-400 text-center text-2xl font-bold'>Chatify</h1>
-      <h3 className='text-sky-300 font-semibold'>Type your message</h3>
+      return () => {
+        socket.emit("disconnect");
+    };
+    
+  }, [socket]);
 
+
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    socket.emit("send-message",message);
+    setMessage('');
+  }
+  return (
+    <div className='flex justify-center items-center flex-col'>
+      <h1 className='text-2xl text-sky-500'>Chat kro</h1>
       <input
         type='text'
-        className='border rounded-lg p-2 font-semibold '
-        placeholder='enter your message'
+        className='border-2 border-sky-500 rounded-md p-2 m-2'
+        placeholder='Type your message...'
+        value={message}
+        onChange={(e)=>{setMessage(e.target.value)}}
       />
+      <button className='border p-2 bg-gray-400 cursor-pointer' type='submit' onClick={handleSubmit}>Submit </button>
     </div>
+
   )
 }
 
-export default App;
+export default App
--- server/index.js
+++ server/index.js
@@ -1,46 +1,32 @@
-    const http = require('http');
-    const express= require('express');
-    const connectDB= require('./config/db');
-    const dotenv = require('dotenv');
-    const cors= require('cors');
-    const {Server} = require('socket.io');
+const express= require('express');
+const http = require('http');
+const app= express();
+const server= http.createServer(app);
+const {Server} = require("socket.io");
+const cors = require('cors');
 
-    const userRoutes = require('./routes/userRoutes');
-    dotenv.config();
-    connectDB();
-    const app= express();
-    app.use(express.json());
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:5173",
+        method:["GET","POST"],
+        credentials:true
+    }
+})
 
-    const server = http.createServer(app);
-    const io= new Server(server,{
-        cors:{
-            origin:"http://localhost:5173",
-            methods:["GET","POST"],
-            credentials: true,
-        }
+
+io.on("connection",(socket)=>{
+    console.log("User connected",socket.id);
+
+    socket.on("send-message",(data)=>{
+        console.log("message recieved",data);
+        io.emit("receive-message",data);
+        
+    })
+    socket.on("disconnect",()=>{
+        console.log("user disconnected" + socket.id);
     });
+})
 
-    app.use(
-        cors({
-            origin: "http://localhost:5173",
-            methods: ["GET", "POST"],
-            credentials: true,
-        })
-    );
-    io.on("connection",(socket)=>{
-        console.log("user connected");
-        console.log("userid", socket.id);
-        // socket.emit("welcome",`welcome to the server ${socket.id}`);
-        socket.broadcast.emit("welcome",`welcome to the server ${socket.id}`);//this will send to all users except the one who connected
-        
-    })
-
-
-   
-
-    app.use('/api', userRoutes);
-    const PORT = process.env.PORT;
-    server.listen(PORT,()=>{
-        console.log(`server is running on port ${process.env.PORT}`);
-        
-    })
+server.listen(3000,()=>{
+    console.log(`server is running on port 3000`);
+})
